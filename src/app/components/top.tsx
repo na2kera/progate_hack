9eb5a7377796664ff72ee960a686d646b41a9f07
Yu220284 <iwase.yu@st.kitasato-u.ac.jp>
2024-05-05 12:08:42 +0900

    投稿フォームの送信ボタン色変更、詳細ページの画像位置、戻るボタンの補正を行いました。

--- src/app/components/top.tsx
+++ src/app/components/top.tsx
@@ -160,7 +160,7 @@ export default function Example() {
         />
         <button
           type="submit"
-          className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="w-full text-white font-bold py-2 px-4 rounded"
         >
           送信
         </button>
