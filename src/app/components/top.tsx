865c296f48cff914cd7e25d21ba2dedbb7d49e03
na2kera <nat3mitaro@gmail.com>
2024-05-05 12:49:23 +0900

    一覧ページに戻るボタンの追加

--- src/app/components/top.tsx
+++ src/app/components/top.tsx
@@ -62,114 +62,124 @@ export default function Example() {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="max-w-lg mx-auto p-4 shadow-lg rounded-lg bg-white"
-    >
-      <label htmlFor="file-upload" className="block mb-4">
-        <span className="text-gray-700">アップロードする</span>
-        <input
-          id="file-upload"
-          name="file-upload"
-          type="file"
-          className="sr-only"
-          accept="image/png, image/jpeg"
-          onChange={(e) => {
-            const fileList = e.target?.files;
-            console.log(fileList);
-            handleUploadStorage(fileList);
-          }}
-        />
-        <img
-          src={path}
-          alt=""
-          width="800"
-          height="500"
-          className="mt-2 rounded"
-        />
-      </label>
-      <div className="space-y-4">
-        <input
-          type="text"
-          placeholder="名前"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="w-full p-2 border rounded"
-        />
-        <textarea
-          placeholder="説明"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          className="w-full p-2 border rounded"
-        />
-        <input
-          type="number"
-          placeholder="年齢"
-          value={age}
-          onChange={(e) => setAge(e.target.value)}
-          className="w-full p-2 border rounded"
-        />
-        <select
-          value={gender}
-          onChange={(e) => setGender(e.target.value)}
-          className="w-full p-2 border rounded"
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-lg mx-auto p-4 shadow-lg rounded-lg bg-white"
+      >
+        <label htmlFor="file-upload" className="block mb-4">
+          <span className="text-gray-700">アップロードする</span>
+          <input
+            id="file-upload"
+            name="file-upload"
+            type="file"
+            className="sr-only"
+            accept="image/png, image/jpeg"
+            onChange={(e) => {
+              const fileList = e.target?.files;
+              console.log(fileList);
+              handleUploadStorage(fileList);
+            }}
+          />
+          <img
+            src={path}
+            alt=""
+            width="800"
+            height="500"
+            className="mt-2 rounded"
+          />
+        </label>
+        <div className="space-y-4">
+          <input
+            type="text"
+            placeholder="名前"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="w-full p-2 border rounded"
+          />
+          <textarea
+            placeholder="説明"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className="w-full p-2 border rounded"
+          />
+          <input
+            type="number"
+            placeholder="年齢"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+            className="w-full p-2 border rounded"
+          />
+          <select
+            value={gender}
+            onChange={(e) => setGender(e.target.value)}
+            className="w-full p-2 border rounded"
+          >
+            <option value="">性別を選択</option>
+            <option value="male">男の子</option>
+            <option value="female">女の子</option>
+            <option value="other">その他</option>
+          </select>
+          <input
+            type="number"
+            placeholder="体重 (kg)"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+            className="w-full p-2 border rounded"
+          />
+          <input
+            type="number"
+            placeholder="体高 (cm)"
+            value={height}
+            onChange={(e) => setHeight(e.target.value)}
+            className="w-full p-2 border rounded"
+          />
+          <select
+            value={dogOrCat}
+            onChange={(e) => setDogOrCat(e.target.value)}
+            className="w-full p-2 border rounded"
+          >
+            <option value="">犬か猫を選択</option>
+            <option value="dog">犬</option>
+            <option value="cat">猫</option>
+          </select>
+          <input
+            type="text"
+            placeholder="種類"
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+            className="w-full p-2 border rounded"
+          />
+          <input
+            type="text"
+            placeholder="所在地"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            className="w-full p-2 border rounded"
+          />
+          <input
+            type="email"
+            placeholder="メールアドレス"
+            value={mail}
+            onChange={(e) => setMail(e.target.value)}
+            className="w-full p-2 border rounded"
+          />
+          <button
+            type="submit"
+            className="w-full bg-gray-700 text-white font-bold py-2 px-4 rounded"
+          >
+            送信
+          </button>
+        </div>
+      </form>
+      <div className="fixed bottom-0 left-0 p-4">
+        <Link
+          href="/"
+          className="bg-[#5B5B5B] hover:bg-[#4A4A4A] text-white font-bold py-2 px-4 rounded-full text-sm"
         >
-          <option value="">性別を選択</option>
-          <option value="male">男の子</option>
-          <option value="female">女の子</option>
-          <option value="other">その他</option>
-        </select>
-        <input
-          type="number"
-          placeholder="体重 (kg)"
-          value={weight}
-          onChange={(e) => setWeight(e.target.value)}
-          className="w-full p-2 border rounded"
-        />
-        <input
-          type="number"
-          placeholder="体高 (cm)"
-          value={height}
-          onChange={(e) => setHeight(e.target.value)}
-          className="w-full p-2 border rounded"
-        />
-        <select
-          value={dogOrCat}
-          onChange={(e) => setDogOrCat(e.target.value)}
-          className="w-full p-2 border rounded"
-        >
-          <option value="">犬か猫を選択</option>
-          <option value="dog">犬</option>
-          <option value="cat">猫</option>
-        </select>
-        <input
-          type="text"
-          placeholder="種類"
-          value={type}
-          onChange={(e) => setType(e.target.value)}
-          className="w-full p-2 border rounded"
-        />
-        <input
-          type="text"
-          placeholder="所在地"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-          className="w-full p-2 border rounded"
-        />
-        <input
-          type="email"
-          placeholder="メールアドレス"
-          value={mail}
-          onChange={(e) => setMail(e.target.value)}
-          className="w-full p-2 border rounded"
-        />
-        <button
-          type="submit"
-          className="w-full bg-gray-700 text-white font-bold py-2 px-4 rounded"
-        >
-          送信
-        </button>
+          一覧に戻る
+        </Link>
       </div>
-    </form>
+    </>
   );
 }
